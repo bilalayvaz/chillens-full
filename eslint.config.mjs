98c17cc99a4ab8e49e4c9cfc0f84ba4e3d461f77
bilalayvazoglu <bilal_ayvazoglu@hotmail.com>
2025-01-09 17:56:54 +0300

    fix: add eslint no-explicit-any rule and set node engine version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,8 @@ const eslintConfig = [
   {
     rules: {
       "react/no-unescaped-entities": "off",
-      "@typescript-eslint/no-unused-vars": "warn"
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off"  // any tipini kullanmaya izin ver
     }
   }
 ];
