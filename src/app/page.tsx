aa9cdda3d404dc04ed496d5637a927359c70af29
bilalayvazoglu <bilal_ayvazoglu@hotmail.com>
2025-01-11 15:54:25 +0300

    wagmin-deleted-mainnet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React, { useEffect } from 'react';
 import Image from 'next/image';
 import Navbar from './components/Navbar';
 import { useSession } from '@lens-protocol/react-web';
--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,6 +1,6 @@
 import { cookieStorage, createStorage } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, polygon } from '@reown/appkit/networks'
+import { polygon } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
@@ -9,7 +9,7 @@ if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [mainnet, polygon]
+export const networks = [polygon]
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
