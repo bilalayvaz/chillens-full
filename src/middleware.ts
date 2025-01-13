4ab86c853c9a068aa9b18c2e0757d6c3b78bf4bd
bilalayvazoglu <bilal_ayvazoglu@hotmail.com>
2025-01-13 17:47:34 +0300

    middlee

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  console.log('Middleware running');
+  console.log('Maintenance mode:', process.env.NEXT_PUBLIC_MAINTENANCE_MODE);
+  console.log('Current path:', request.nextUrl.pathname);
+  
   const token = request.cookies.get('token')?.value
   const { pathname } = request.nextUrl
 
