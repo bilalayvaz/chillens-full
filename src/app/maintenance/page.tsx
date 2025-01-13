e91d0079c49e42d08b25a84fd780d616e0752fa9
bilalayvazoglu <bilal_ayvazoglu@hotmail.com>
2025-01-13 14:45:51 +0300

    maintenance

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+    return (
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>Bakım Modu</h1>
+        <p>Şu anda sistemimizde bakım yapılıyor. Lütfen daha sonra tekrar deneyin.</p>
+      </div>
+    );
+  }
